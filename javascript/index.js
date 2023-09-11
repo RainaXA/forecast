7322538a71199b83b3d721a76f67a490edb93696
Raina <101020268+RainaXA@users.noreply.github.com>
2023-09-11 15:16:07 -0700

    Can now load from any folder, begin console commands

--- javascript/index.js
+++ javascript/index.js
@@ -8,8 +8,8 @@ global.rl = readline.createInterface({
 });
 
 global.version = {
-  name: "Vesta",
-  string: "1.2.0"
+  name: "Saturn",
+  string: "1.3.0-dev"
 }
 
 try {
@@ -28,6 +28,8 @@ global.sources = {
   core: "forecast",
   modules: "modules"
 }
+
+let loaded;
 
 global.logging = {
   error: 91,
@@ -79,6 +81,33 @@ global.forecast = { // forecast functions - keep it in one object for low interf
 	}
 }
 
+function loadModules(path) { // load all modules in this path
+	fs.readdir(path, function(error, files) {
+		if(!files) return log("directory does not exist", logging.error, sources.core);
+		let modules = files.filter(f => f.split(".").pop() === "js");
+		let counter = 0;
+		modules.forEach((f, i) => {
+		  try {
+			let props = require(`${path}/${f}`);
+			if(!props.info) {
+				log(f + " does not have info (pre-1.2.0?)", logging.warn, sources.modules)
+			} else {
+				if(!props.info.name || !props.info.desc || !props.info.ver || !props.info.fVer) return log(f + " has an invalid info page - must contain name, desc, ver, and fVer", logging.warn, sources.modules)
+				let forecastVersion = props.info.fVer;
+				if (parseInt(forecastVersion.split(".")[1]) + 4 <= parseInt(version.string.split(".")[1]) && !settings.hideCompatibility) log(props.info.name + " is older module [" + forecastVersion + " module running on forecast " + version.string + "]", logging.warn, sources.modules)
+				if (parseInt(forecastVersion.split(".")[1]) > parseInt(version.string.split(".")[1]) && !settings.hideCompatibility) log(props.info.name + " is newer than current forecast [" + forecastVersion + " module running on forecast " + version.string + "]", logging.warn, sources.modules)
+			}
+		  } catch (err) {
+			log(err.stack, logging.error, sources.modules);
+			counter++;
+		  }
+		})
+		if(settings.doNotLogStartup != 2)  log("loaded " + (modules.length - counter) + " modules", logging.success, sources.modules);
+		loaded = path;
+		if(settings.doNotLogStartup != 1 && settings.doNotLogStartup != 2)  log("started on version " + version.string + " " + version.name, logging.success, sources.core);
+	})
+}
+
 for(const override in settings.overrides) {
 	if(isNaN(override)) settings.overrides[logging[override]] = settings.overrides[override] // set the number of a logging type as an override directly to be interpreted by log()
 }
@@ -98,34 +127,51 @@ global.log = function(message, type, sender) {
 if(settings.doNotLogStartup != 1 && settings.doNotLogStartup != 2) log("starting", logging.success, sources.core); // this is the only way i can get this bullshit to work currently and i am impatient
 if(!fs.existsSync("./settings.json")) log("settings.json does not exist. you can create it to change some basic functionality of forecast!", logging.info, sources.core)
 
-fs.readdir("./modules/", function(error, files) {
-  if (error) {
-    fs.mkdirSync("./modules/");
-    if(settings.doNotLogStartup != 2) log("folder not found, creating now", logging.warn, sources.modules);
-  } else {
-    let modules = files.filter(f => f.split(".").pop() === "js");
-    let counter = 0;
-    modules.forEach((f, i) => {
-      try {
-        let props = require(`./modules/${f}`);
-		if(!props.info) {
-			log(f + " does not have info (pre-1.2.0?)", logging.warn, sources.modules)
-		} else {
-			if(!props.info.name || !props.info.desc || !props.info.ver || !props.info.fVer) return log(f + " has an invalid info page - must contain name, desc, ver, and fVer", logging.warn, sources.modules)
-			let forecastVersion = props.info.fVer;
-			if (parseInt(forecastVersion.split(".")[1]) + 4 <= parseInt(version.string.split(".")[1]) && !settings.hideCompatibility) log(props.info.name + " is older module [" + forecastVersion + " module running on forecast " + version.string + "]", logging.warn, sources.modules)
-			if (parseInt(forecastVersion.split(".")[1]) > parseInt(version.string.split(".")[1]) && !settings.hideCompatibility) log(props.info.name + " is newer than current forecast [" + forecastVersion + " module running on forecast " + version.string + "]", logging.warn, sources.modules)
+var directories = [];
+if(!settings.defaultPath) {
+	log("searching for directories", logging.success, sources.core);
+	fs.readdir("./", function(error, files) {
+		files.forEach((f, i) => {
+			if(fs.lstatSync("./" + f).isDirectory()) directories.push("./" + f);
+		})
+		if(directories.length == 0) {
+			fs.mkdirSync("./modules/");
+			if(settings.doNotLogStartup != 2) log("no directory found, creating ./modules/", logging.warn, sources.modules);
 		}
-      } catch (err) {
-        log(err.stack, logging.error, sources.modules);
-        counter++;
-      }
-    })
-    if(settings.doNotLogStartup != 2)  log("loaded " + (modules.length - counter) + " modules", logging.success, sources.modules);
-  }
-  if(settings.doNotLogStartup != 1 && settings.doNotLogStartup != 2)  log("started on version " + version.string + " " + version.name, logging.success, sources.core);
+		if(directories.length == 1) { // load the only directory
+			log("loading from only directory", logging.success, sources.core);
+			loadModules(directories[0]);
+		} else {
+			log("multiple directories found, please input the directory to load from", logging.info, sources.core)
+			let dirs = "";
+			for(let dir of directories) {
+				dirs = dirs + dir.slice(2) + "\n";
+			}
+			rl.question(dirs + "\ndirectory: ", (input) => {
+				let find = directories.findIndex(dir => input == dir.slice(2));
+				loadModules(directories[find]);
+			})
+		}
+	})
+} else {
+	log("loading from " + settings.defaultPath + " (default path)", logging.success, sources.core);
+	loadModules(settings.defaultPath);
+}
+
+rl.on('line', function(input) {
+	if(!input.toLowerCase().startsWith("fc ")) return; // we only want stuff that begins with fc
+	input = input.toLowerCase().slice(3) // remove "fc "
+	let args = input.split(" ");
+	input = args[0];
+	args.shift();
+	switch(input.toLowerCase()) {
+		case "load":
+		case "unload":
+		case "about":
+			log("Forecast " + version.string + " " + version.name + "\nfront-end framework built for modularity\n\nhttps://github.com/rainaxa/forecast", logging.info);
+	}
 })
 
 process.on('uncaughtException', function(err) {
-	log(err, logging.error)
+	log(err.stack, logging.error)
 })
