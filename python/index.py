1216b52438e6580d5d4cff58778bd0cb8cae4c44
Raina <101020268+RainaXA@users.noreply.github.com>
2023-09-12 22:28:31 -0700

    Fix bug where Forecast would crash if no modules and where modules would execute Forecast code

--- python/index.py
+++ python/index.py
@@ -4,7 +4,7 @@ os.system("")  # enables ansi escape characters in terminal
 
 class ver:
     name = "Neptune"
-    string = "1.0.0-rc1"
+    string = "1.0.0"
 version = ver()
 
 title = "Forecast " + version.string + " " + version.name # other modules may modify it
@@ -29,24 +29,25 @@ def log(message, type, sender=None):
     else:
         print('\033[' + type + 'm' + sender + ": " + message + '\033[0m')
 
-#
-log("starting", logging.success, sources.core)
 
-if not os.path.exists("./modules"):
-    os.mkdir("modules")
-    log("folder not found, creating now", logging.warn, sources.modules)
+if __name__ == "__main__": # so that modules dont infinitely execute this
+    log("starting", logging.success, sources.core)
 
-counter = 0
-commands = []
-for files in os.listdir('modules'):
-    if(files.endswith(".py")):
-        counter = counter + 1
-        commands.append("python ./modules/" + files)
-    else:
-        continue
-log("loaded " + str(counter) + " modules", logging.success, sources.modules)
-log("started on version " + version.string + " " + version.name, logging.success, sources.core)
-for j in range(max(int(len(commands)/counter), 1)):
-    procs = [subprocess.Popen(i, shell=True) for i in commands[j*counter: min((j+counter)*counter, len(commands))] ]
-    for p in procs:
-        p.wait()
+    if not os.path.exists("./modules"):
+        os.mkdir("modules")
+        log("folder not found, creating now", logging.warn, sources.modules)
+    counter = 0
+    commands = []
+    for files in os.listdir('modules'):
+        if(files.endswith(".py")):
+            counter = counter + 1
+            commands.append("python ./modules/" + files)
+        else:
+            continue
+    log("loaded " + str(counter) + " modules", logging.success, sources.modules)
+    log("started on version " + version.string + " " + version.name, logging.success, sources.core)
+    if not counter == 0:
+        for j in range(max(int(len(commands)/counter), 1)):
+            procs = [subprocess.Popen(i, shell=True) for i in commands[j*counter: min((j+counter)*counter, len(commands))] ]
+            for p in procs:
+                p.wait()
